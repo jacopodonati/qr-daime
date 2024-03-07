749b898685ad32d4d4ccdaac9307ca5febbd56b3
Jacopo Donati <33698919+jacopodonati@users.noreply.github.com>
2024-03-07 14:49:30 +0100

    Add QR Code generator

--- routes/doc.js
+++ routes/doc.js
@@ -1,6 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 const { getClient, ObjectId } = require('../db');
+const QRCode = require('qrcode');
+const { domain } = require('../config');
+
+QR_CODE_DIR = './static/qr/'
+
+// TODO: check if `QR_CODE_DIR` exists and make it.
+
+async function getQRCode(hash, url) {
+    const qrCodePath = QR_CODE_DIR + hash + '.png';
+
+    return new Promise((resolve, reject) => {
+        fs.access(qrCodePath, fs.constants.F_OK, async (err) => {
+            if (err) {
+                try {
+                    await QRCode.toFile(qrCodePath, url);
+                    resolve(qrCodePath);
+                } catch (error) {
+                    reject(error);
+                }
+            } else {
+                resolve(qrCodePath);
+            }
+        });
+    });
+}
 
 router.get('/:hash', async (req, res) => {
     const hash = req.params.hash;
@@ -12,9 +38,13 @@ router.get('/:hash', async (req, res) => {
         const document = await collection.findOne({ _id: new ObjectId(hash) });
 
         if (document) {
+            const fullURL = `${domain}${req.originalUrl}`;
+            console.log(fullURL)
+            const qrCodePath = await getQRCode(hash, fullURL);
             res.render('doc', {
                 title: 'Issue: ' + document._id,
-                document: document
+                document: document,
+                qr: qrCodePath
             });
         } else {
             res.redirect('/list');
@@ -25,5 +55,7 @@ router.get('/:hash', async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
+
+router.use('/static', express.static('static'));
 
 module.exports = router;
