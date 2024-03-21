48ce11b3ba341e245552544f8c5f31dc7765ff6d
Jacopo Donati <33698919+jacopodonati@users.noreply.github.com>
2024-03-21 15:07:04 +0100

    Migration to mongoose

--- routes/list.js
+++ routes/list.js
@@ -1,23 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const { getClient, ObjectId } = require('../db');
+const Document = require('../models/document');
 const i18n = require('i18n');
 
 router.get('/', async (req, res) => {
     try {
-        const client = await getClient(); 
-        const database = client.db('da1me');
-        const collection = database.collection('documents');
-
-        const documents = await collection.find({}).toArray();
+        const documents = await Document.find({});
+        
         res.render('list', {
             title: i18n.__('listpage_title') + ' - ' + i18n.__('app_name'),
             documents: documents
         });
 
     } catch (error) {
-        console.error('Error querying database:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        console.error('Errore durante la query al database:', error);
+        res.status(500).json({ error: 'Errore del server interno' });
     }
 });
 
