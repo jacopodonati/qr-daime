b55f99b64b75e96c4ae3584f318b4c2591752c31
Jacopo Donati <33698919+jacopodonati@users.noreply.github.com>
2024-03-21 09:10:53 +0100

    Migration to Mongoose

--- routes/doc.js
+++ routes/doc.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const Document = require('../models/document');
 const fs = require('fs');
-const { getClient, ObjectId } = require('../db');
 const i18n = require('i18n');
 const QRCode = require('qrcode');
 const { domain } = require('../config');
@@ -32,15 +32,10 @@ async function getQRCode(hash, url) {
 router.get('/:hash', async (req, res) => {
     const hash = req.params.hash;
     try {
-        const client = await getClient(); 
-        const database = client.db('da1me');
-        const collection = database.collection('documents');
-
-        const document = await collection.findOne({ _id: new ObjectId(hash) });
+        const document = await Document.findById(hash);
 
         if (document) {
             const fullURL = `${domain}${req.originalUrl}`;
-            console.log(fullURL)
             const qrCodePath = await getQRCode(hash, fullURL);
             res.render('doc', {
                 title: i18n.__("document") + ': ' + document._id + ' - ' + i18n.__('app_name'),
@@ -50,7 +45,6 @@ router.get('/:hash', async (req, res) => {
         } else {
             res.redirect('/list');
         }
-
     } catch (error) {
         console.error('Error querying database:', error);
         res.status(500).json({ error: 'Internal Server Error' });
