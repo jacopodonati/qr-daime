c2d6b3c5054b069fe549a87e3def479749e1e8de
Jacopo Donati <33698919+jacopodonati@users.noreply.github.com>
2024-03-22 16:09:53 +0100

    Add navbar

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ const i18n = require('i18n');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title: i18n.__('homepage_title') + ' - ' + i18n.__('app_name')
+    title: i18n.__('homepage_title') + ' - ' + i18n.__('app_name'),
+    hideNavbar: true
   });
 });
 
