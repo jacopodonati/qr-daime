697c7792a6fce3f6d9889fabd3d92a96b15110eb
Jacopo Donati <33698919+jacopodonati@users.noreply.github.com>
2024-03-25 10:39:47 +0100

    Add QR Code with all data inside the single doc view

--- routes/doc.js
+++ routes/doc.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Document = require('../models/document');
+const Information = require('../models/information');
 const fs = require('fs');
 const i18n = require('i18n');
 const QRCode = require('qrcode');
@@ -8,6 +9,8 @@ const { domain } = require('../config');
 
 QR_DIR = './static/qr/';
 QR_LINK_DIR = 'link/';
+QR_DOC_DIR = 'doc/';
+QR_DOC_LOCALE = 'en';
 
 function createQRDirectory() {
     try {
@@ -19,10 +22,60 @@ function createQRDirectory() {
             fs.mkdirSync(QR_DIR + QR_LINK_DIR);
         }
 
+        if (!fs.existsSync(QR_DIR + QR_DOC_DIR)) {
+            fs.mkdirSync(QR_DIR + QR_DOC_DIR);
+        }
     } catch (err) {
         console.error('Si è verificato un errore durante la creazione delle cartelle per i QR:', err);
     }
 }
+
+async function getQRDocument(document) {
+    createQRDirectory();
+    const qrCodePath = QR_DIR + QR_DOC_DIR + document._id + '.png';
+
+    let text = `Document no. ${document._id}
+Issued on: ${formatDate(document.dateOfIssue)}
+Modified on: ${formatDate(document.lastEdit)}
+Informations:`;
+
+    for (const info of document.information) {
+        const information = await Information.findById(info._id);
+        const informationLabel = information.labels.find(label => label.locale === QR_DOC_LOCALE).text;
+        text += `
+- ${informationLabel}`
+        for (const field of info.fields) {
+            const fieldLabel = information.fields.find(field => field._id === field._id).labels.find(label => label.locale === QR_DOC_LOCALE).text;
+            text += `
+  - ${fieldLabel}: ${field.value}`
+        }
+    }
+
+    return new Promise((resolve, reject) => {
+        fs.access(qrCodePath, fs.constants.F_OK, async (err) => {
+            if (err) {
+                try {
+                    await QRCode.toFile(qrCodePath, text);
+                    resolve(qrCodePath);
+                } catch (error) {
+                    reject(error);
+                }
+            } else {
+                resolve(qrCodePath);
+            }
+        });
+    });
+}
+
+const formatDate = (dateString) => {
+    const options = { day: 'numeric', month: 'long', year: 'numeric', hour: '2-digit', minute: '2-digit', hour12: false };
+    const formattedDate = new Date(dateString).toLocaleString(QR_DOC_LOCALE, options);
+    return formattedDate;
+};
+
+async function getQRLink(hash, url) {
+    createQRDirectory();
+    const qrCodePath = QR_DIR + QR_LINK_DIR + hash + '.png';
 
     return new Promise((resolve, reject) => {
         fs.access(qrCodePath, fs.constants.F_OK, async (err) => {
@@ -54,10 +107,12 @@ router.get('/:hash', async (req, res) => {
         if (document) {
             const fullURL = `${domain}${req.originalUrl}`;
             const qrLinkPath = await getQRLink(hash, fullURL);
+            const qrDocPath = await getQRDocument(document);
             res.render('doc', {
                 title: i18n.__("document") + ': ' + document._id + ' - ' + i18n.__('app_name'),
                 document: document,
                 qr_link: qrLinkPath,
+                qr_doc: qrDocPath,
                 isAdmin
             });
         } else {
