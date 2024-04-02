5635e8d8642f852bf54bc36568768558e2b726cf
Jacopo Donati <33698919+jacopodonati@users.noreply.github.com>
2024-04-02 14:53:51 +0200

    First support for Netlify

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const i18n = require('i18n');
+const serverless = require('serverless-http');
 
 require('dotenv').config();
 
@@ -43,20 +44,24 @@ app.use('/edit', editRouter);
 app.use('/field', fieldRouter);
 
 app.use(function (req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 app.use(function (err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  res.status(err.status || 500);
-  res.render('error');
+    res.status(err.status || 500);
+    res.render('error');
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Port: ${port}`);
-});
+if (process.env.MODE === 'local') {
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+        console.log(`Port: ${port}`);
+    });
 
-module.exports = app;
+    module.exports = app;
+} else {
+    module.exports.handler = serverless(app);
+}
---
+++ netilfy/api.js
@@ -0,0 +1,5 @@
+const app = require('./app');
+
+exports.handler = async (event, context) => {
+    return await app(event, context);
+};
