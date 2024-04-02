11c3a9900fe3242c77b6f02632ea6451a6e1c158
Jacopo Donati <33698919+jacopodonati@users.noreply.github.com>
2024-04-02 15:26:18 +0200

    Routing test

--- netlify/functions/api/api.ts
+++ netlify/functions/api/api.ts
@@ -1,11 +1,14 @@
 import express, { Router } from "express";
 import serverless from "serverless-http";
+const app = require('../../../app');
 
 const api = express();
+const indexRouter = require('./routes/index');
 
 const router = Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
 api.use("/api/", router);
+api.use("/idx/", indexRouter);
 
 export const handler = serverless(api);
