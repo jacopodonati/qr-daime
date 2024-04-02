9cfdd6a1b75d0f4239a87a63999ea03f420cb659
Jacopo Donati <33698919+jacopodonati@users.noreply.github.com>
2024-04-02 14:15:31 +0200

    Fix cosmetics

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ const app = express();
 
 app.use(i18n.init);
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -43,18 +42,14 @@ app.use('/add', addRouter);
 app.use('/edit', editRouter);
 app.use('/field', fieldRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
