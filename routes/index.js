cd7c3b45b4cb9d0de79b8008caf6a1ad2dc2d716
Jacopo Donati <33698919+jacopodonati@users.noreply.github.com>
2024-04-19 11:52:03 +0200

    Remove leftovers

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,9 @@ const express = require('express');
 const router = express.Router();
 const i18n = require('i18n');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title: i18n.__('homepage_title') + ' - ' + i18n.__('app_name'),
-    hideNavbar: false
+    title: i18n.__('homepage_title') + ' - ' + i18n.__('app_name')
   });
 });
 
