b16ef78750de002df02c58c1be29ed860bcf150f
Jacopo Donati <33698919+jacopodonati@users.noreply.github.com>
2024-05-13 17:37:15 +0200

    Aggiunta la modifica dei template

---
+++ routes/templates/edit.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const router = express.Router();
+const i18n = require('i18n');
+const User = require('../../models/user');
+const Template = require('../../models/template');
+const Workspace = require('../../models/workspace');
+const Information = require('../../models/information');
+
+router.get('/:id', async function(req, res, next) {
+    if (!res.locals.user.permissions.manage_documents) {
+        return res.status(403).send('Operazione non consentita');
+    }
+
+    try {
+        const user = await User.findById(res.locals.user.id);
+        let workspacesIds = [];
+        if (user) {
+            workspacesIds = user.workspaces.map(workspace => workspace._id);
+        }
+        let template;
+        if (res.locals.user.permissions.manage_documents) {
+            template = await Template.findOne({_id: req.params.id});
+        } else {
+            template = await Template.findOne({
+                $or: [
+                    { _id: id, deleted: false },
+                    { $and: [{ $or: [{ owner: res.locals.user.id }, { workspace: { $in: workspacesIds } }] }] }
+                ]
+            });
+        }
+        const workspaces = await Workspace.find({ 'members.user': res.locals.user.id });
+        const fields = await Information.find({ deleted: false });
+
+        if (template) {
+            res.render('templates/edit', {
+                title: i18n.__('template_edit_title') + ' - ' + i18n.__('app_name'),
+                template,
+                workspaces,
+                fields
+            });
+        }
+    } catch (error) {
+        console.error('Errore durante il recupero dei campi:', error);
+        next(error);
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const { id, title, workspace, info } = req.body;
+
+        const user = await User.findById(res.locals.user.id);
+        let workspacesIds = [];
+        if (user) {
+            workspacesIds = user.workspaces.map(workspace => workspace._id);
+        }
+        let template;
+        if (res.locals.user.permissions.manage_documents) {
+            template = await Template.findOne({_id: id});
+        } else {
+            template = await Template.findOne({
+                $or: [
+                    { _id: id, deleted: false },
+                    { $and: [{ $or: [{ owner: res.locals.user.id }, { workspace: { $in: workspacesIds } }] }] }
+                ]
+            });
+        }
+        
+        if (template) {
+            template.title = title;
+            template.workspace = workspace;
+            template.info = info;
+            const savedTemplate = await template.save();
+            res.status(201).json({ id: id });
+        } else {
+            res.status(404);
+        }
+    } catch (error) {
+        console.error('Errore durante il salvataggio del template:', error);
+        res.status(500).json({ error: 'Errore durante il salvataggio del template' });
+    }
+});
+
+module.exports = router;
