322efebbfa2bca9379425e0e224baa255fb53d0e
Jacopo Donati <33698919+jacopodonati@users.noreply.github.com>
2024-06-19 15:21:14 +0200

    Fix lista template

--- routes/templates/list.js
+++ routes/templates/list.js
@@ -5,10 +5,76 @@ const i18n = require('i18n');
 
 router.get('/', async (req, res) => {
     try {
-        const queryString = res.locals.user.permissions.manage_documents ? {} : { deleted: false };
-        const templates = await Template.find(queryString)
-            .populate('owner workspace');
-        
+        let templates;
+        if (res.locals.user.permissions.manage_documents) {
+            templates = await Template.find({})
+            .populate({
+                path: 'owner',
+                select: '_id username'
+            })
+            .populate({
+                path: 'workspace',
+                select: 'privacy name'
+            });
+        } else {
+            templates = await Template.aggregate([
+                { 
+                    $match: 
+                        { $and:
+                            [
+                                { deleted: false },
+                                { $or: 
+                                    [
+                                        { owner: res.locals.user._id },
+                                        { workspace: { $exists: true } }
+                                    ] 
+                                }
+                            ]
+                        }
+                    },
+                {
+                    $lookup: {
+                        from: 'workspaces',
+                        localField: 'workspace',
+                        foreignField: '_id',
+                        as: 'workspaceDetails'
+                    }
+                },
+                { $unwind: '$workspaceDetails' },
+                {
+                    $lookup: {
+                        from: 'users',
+                        localField: 'owner',
+                        foreignField: '_id',
+                        as: 'ownerDetails'
+                    }
+                },
+                { $unwind: '$ownerDetails' },
+                {
+                    $match: {
+                        $or: [
+                            { 'workspaceDetails.members.user': res.locals.user._id },
+                            { 'workspaceDetails.privacy': 'public' }
+                        ]
+                    }
+                },
+                {
+                    $project: {
+                        _id: 1,
+                        owner: {
+                            _id: '$ownerDetails._id',
+                            username: '$ownerDetails.username'
+                        },
+                        'workspace._id': '$workspaceDetails._id',
+                        'workspace.name': '$workspaceDetails.name',
+                        'workspace.privacy': '$workspaceDetails.privacy',
+                        title: 1
+                    }
+                }
+            ]
+            );
+        }
+
         res.render('templates/list', {
             title: i18n.__('template_list_title') + ' - ' + i18n.__('app_name'),
             templates
