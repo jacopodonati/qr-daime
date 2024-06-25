943cffcb6cf8ee690226a0300dc5d84edad14ce6
Jacopo Donati <33698919+jacopodonati@users.noreply.github.com>
2024-06-25 15:26:01 +0200

    Aggiunta visualizzazione multilingua del doc

--- routes/documents/single.js
+++ routes/documents/single.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Document = require('../../models/document');
 const i18n = require('i18n');
+const iso6391 = require('iso-639-1');
 const { replaceUrlWithImg } = require('../../middleware/validation');
 
 router.get('/', async (req, res) => {
@@ -11,10 +12,30 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     try {
         const id = req.params.id;
+        const locale_codes = i18n.getLocales();
+        let availableLocales = []
+        let qrLabels = []
+        locale_codes.forEach(code => {
+            const name = iso6391.getNativeName(code);
+            availableLocales.push({ code, name })
+            qrLabels.push({code, label: 'record_no', value: i18n.__({ phrase: 'record_no', locale: code})});
+            qrLabels.push({code, label: 'record_date_issue', value: i18n.__({ phrase: 'record_date_issue', locale: code})});
+            qrLabels.push({code, label: 'record_date_edit', value: i18n.__({ phrase: 'record_date_edit', locale: code})});
+            qrLabels.push({code, label: 'record_information', value: i18n.__({ phrase: 'record_information', locale: code})});
+            
+            // !{doc
+            //     "record_date_issue")}:
+            //     "record_date_edit")}: 
+            //     "record_information")}})})
+        });
+
+
         const queryString = res.locals.user.permissions.manage_documents ? { _id: id } : { _id: id, deleted: false };
-        const document = await Document.findOne(queryString);
+        let document = await Document.findOne(queryString);
 
         if (document) {
+            await document.populate('information._id');
+            
             for (let i = 0; i < document._doc.information.length; i++) {
                 for (let j = 0; j < document._doc.information[i].fields.length; j++) {
                     const field = document._doc.information[i].fields[j];
@@ -23,10 +44,28 @@ router.get('/:id', async (req, res) => {
                 }
             }
             
+            document = document.toObject();
+            document.information = document.information.map(info => {
+                return {
+                    _id: info._id._id,
+                    public: info.public,
+                    labels: info._id.labels,
+                    descriptions: info._id.descriptions,
+                    fields: info.fields.map(field => {
+                        const matchingField = info._id.fields.find(f => f._id.equals(field._id));
+                        if (matchingField) {
+                            return { ...matchingField, value: field.value };
+                        }
+                        return field;
+                    })
+                };
+            });
+            
             res.render('documents/single', {
                 title: i18n.__("document") + ': ' + document._id + ' - ' + i18n.__('app_name'),
                 document,
-                link: process.env.DOMAIN + '/doc/' + document._id
+                availableLocales,
+                qrLabels
             });
         } else {
             res.redirect('/doc/list');
