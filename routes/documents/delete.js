ca70a631d31dee4ddabbcd8196e5969a8a4894dd
Jacopo Donati <33698919+jacopodonati@users.noreply.github.com>
2024-07-02 09:29:29 +0200

    Fix bug per aggiornamento view doc

--- routes/documents/delete.js
+++ routes/documents/delete.js
@@ -2,17 +2,55 @@ const express = require('express');
 const router = express.Router();
 const Document = require('../../models/document');
 const Workspace = require('../../models/workspace');
+const i18n = require('i18n');
+const { replaceUrlWithImg } = require('../../middleware/validation');
 
 router.get('/:id', async (req, res) => {
     try {
         const id = req.params.id;
+        const locale_codes = i18n.getLocales();
+        let qrLabels = []
+        locale_codes.forEach(code => {
+            qrLabels.push({code, label: 'record_no', value: i18n.__({ phrase: 'record_no', locale: code})});
+            qrLabels.push({code, label: 'record_date_issue', value: i18n.__({ phrase: 'record_date_issue', locale: code})});
+            qrLabels.push({code, label: 'record_date_edit', value: i18n.__({ phrase: 'record_date_edit', locale: code})});
+            qrLabels.push({code, label: 'record_information', value: i18n.__({ phrase: 'record_information', locale: code})});
+        });
         const queryString = res.locals.user.permissions.manage_documents ? { _id: id, deleted: false } : { _id: id, owner: res.locals.user._id, deleted: false };
-        const document = await Document.findOne(queryString);
+        let document = await Document.findOne(queryString);
 
         if (document) {
+            await document.populate('information._id');
+            
+            for (let i = 0; i < document._doc.information.length; i++) {
+                for (let j = 0; j < document._doc.information[i].fields.length; j++) {
+                    const field = document._doc.information[i].fields[j];
+                    let formattedValue = await replaceUrlWithImg(field.value)
+                    field.set('value', formattedValue);
+                }
+            }
+            
+            document = document.toObject();
+            document.information = document.information.map(info => {
+                return {
+                    _id: info._id._id,
+                    public: info.public,
+                    labels: info._id.labels,
+                    descriptions: info._id.descriptions,
+                    fields: info.fields.map(field => {
+                        const matchingField = info._id.fields.find(f => f._id.equals(field._id));
+                        if (matchingField) {
+                            return { ...matchingField, value: field.value };
+                        }
+                        return field;
+                    })
+                };
+            });
+
             res.render('documents/delete', {
                 title: 'document',
-                document
+                document,
+                qrLabels
             });
         } else {
             res.redirect('/doc/list');
