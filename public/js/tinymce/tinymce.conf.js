84e53718289476a1c2dbfcde7f061b82086f938f
Jacopo Donati <33698919+jacopodonati@users.noreply.github.com>
2024-07-04 11:26:45 +0200

    Localizzazione di tinymce

--- public/js/tinymce/tinymce.conf.js
+++ public/js/tinymce/tinymce.conf.js
@@ -1,5 +1,9 @@
+const langDir = '/js/tinymce/langs';
+
 var tinyMCEConfig = {
     license_key: 'gpl',
+    language: globalLocale,
+    language_url: `${langDir}/${globalLocale}.js`,
     promotion: false,
     plugins: 'link image code',
     toolbar: 'undo redo | formatselect | bold italic | alignleft aligncenter alignright | link image | code',
